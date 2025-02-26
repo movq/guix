bf50cd7363c123398c57787153617c65e57db597
Mike Jones <mike@mjones.io>
2025-02-26 08:35:36 +0000

    gnu: rust-quote-1: Update to 1.0.38.
    * gnu/packages/crates-io.scm (rust-quote-1): Update to 1.0.38.
    
    Change-Id: Icd7dae0185b0492b97f318b43fde474432eb15c6

--- gnu/packages/crates-io.scm
+++ gnu/packages/crates-io.scm
@@ -62789,14 +62789,14 @@ transport protocol in Rust.")
 (define-public rust-quote-1
   (package
     (name "rust-quote")
-    (version "1.0.37")
+    (version "1.0.38")
     (source (origin
               (method url-fetch)
               (uri (crate-uri "quote" version))
               (file-name (string-append name "-" version ".tar.gz"))
               (sha256
                (base32
-                "1brklraw2g34bxy9y4q1nbrccn7bv36ylihv12c9vlcii55x7fdm"))))
+                "1k0s75w61k6ch0rs263r4j69b7vj1wadqgb9dia4ylc9mymcqk8f"))))
     (build-system cargo-build-system)
     (arguments
      `(#:cargo-inputs
