6fb411124e5ce34f22f5789d39a5efea2c822127
Mike Jones <mike@mjones.io>
2025-02-26 10:43:54 +0000

    gnu: Add rust-notify-types-2.
    * gnu/packages/crates-io.scm (rust-notify-types-2): New variable.
    
    Change-Id: I57ceea71e0516251d613677e2224746586dc6990

--- gnu/packages/crates-io.scm
+++ gnu/packages/crates-io.scm
@@ -48853,6 +48853,29 @@ client and server")
 Rust dbus client and server.")
     (license (list license:expat license:asl2.0))))
 
+(define-public rust-notify-types-2
+  (package
+    (name "rust-notify-types")
+    (version "2.0.0")
+    (source
+     (origin
+       (method url-fetch)
+       (uri (crate-uri "notify-types" version))
+       (file-name (string-append name "-" version ".tar.gz"))
+       (sha256
+        (base32 "0pcjm3wnvb7pvzw6mn89csv64ip0xhx857kr8jic5vddi6ljc22y"))))
+    (build-system cargo-build-system)
+    (arguments
+     `(#:cargo-inputs (("rust-serde" ,rust-serde-1)
+                       ("rust-web-time" ,rust-web-time-1))
+       #:cargo-development-inputs (("rust-insta" ,rust-insta-1)
+                                   ("rust-rstest" ,rust-rstest-0.24)
+                                   ("rust-serde-json" ,rust-serde-json-1))))
+    (home-page "https://github.com/notify-rs/notify")
+    (synopsis "Types used by the notify crate")
+    (description "This package provides types used by the notify crate.")
+    (license (list license:expat license:asl2.0))))
+
 (define-public rust-notmuch-0.8
   (package
     (name "rust-notmuch")
