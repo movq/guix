c9f4b8ebdeade98d8e4f59c048a305d2c045c2b8
Mike Jones <mike@mjones.io>
2025-02-27 17:06:43 +0000

    gnu: librewolf: Fix missing newlines when generating mozconfig.
    These were being concatenated on a single line together, probably
    causing them to have no effect.
    
    * gnu/packages/librewolf.scm (librewolf)[phases]: Add missing newlines
    to last two entries in mozconfig.
    
    Change-Id: I12e7e33d9c4f9269ea5b0b24d276289f6f47890a

--- gnu/packages/librewolf.scm
+++ gnu/packages/librewolf.scm
@@ -455,9 +455,9 @@
                              (display "mk_add_options MOZ_CRASHREPORTER=0\n")
                              (display "mk_add_options MOZ_DATA_REPORTING=0\n")
                              (display
-                              "mk_add_options MOZ_SERVICES_HEALTHREPORT=0")
+                              "mk_add_options MOZ_SERVICES_HEALTHREPORT=0\n")
                              (display
-                              "mk_add_options MOZ_TELEMETRY_REPORTING=0")))
+                              "mk_add_options MOZ_TELEMETRY_REPORTING=0\n")))
                          (setenv "MOZCONFIG" mozconfig))
                        (invoke "./mach" "configure")))
                    (add-before 'build 'fix-addons-placeholder
