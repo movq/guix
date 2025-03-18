e3bca8278f86ad525d260dfb4d61d30732b15860
Mike Jones <mike@mjones.io>
2025-03-18 16:01:50 +0000

    gnu: golang: Disable tests.
    Change-Id: I29ebefd71ce136b7c617dd061401bca026ccfadc

--- gnu/packages/golang.scm
+++ gnu/packages/golang.scm
@@ -223,8 +223,7 @@
                (setenv "GOOS" "linux")
                (setenv "GOROOT" (dirname (getcwd)))
                (setenv "GOROOT_FINAL" output)
-               (setenv "GO14TESTS" "1")
-               (invoke "sh" "all.bash"))))
+               (invoke "sh" "make.bash"))))
 
          (replace 'install
            (lambda* (#:key outputs inputs #:allow-other-keys)
@@ -521,7 +520,6 @@ in the style of communicating sequential processes (@dfn{CSP}).")
                   (guix build utils))
        ;; TODO: Disable the test(s) in misc/cgo/test/cgo_test.go
        ;; that cause segfaults in the test suite.
-       #:tests? ,(not (or (target-aarch64?) (target-riscv64?)))
        #:phases
        (modify-phases %standard-phases
          (replace 'configure
