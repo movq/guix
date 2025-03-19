25144db12b4dab2b3b526d2cc9ffbda81af4e29f
Mike Jones <mike@mjones.io>
2025-03-19 18:05:16 +0000

    gnu: python-webencodings: Add python-setuptools.
    Change-Id: I1e07334cd34f19096f53f502d04022e248c746e5

--- gnu/packages/python-web.scm
+++ gnu/packages/python-web.scm
@@ -2928,7 +2928,7 @@ desired
              (invoke "py.test" "-v" "webencodings/tests.py")
              #t)))))
     (native-inputs
-     (list python-pytest))
+     (list python-pytest python-setuptools))
     (home-page "https://github.com/SimonSapin/python-webencodings")
     (synopsis "Character encoding aliases for legacy web content")
     (description
