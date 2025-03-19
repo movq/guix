091e3e0d6274430ba89e0d65cef6aabdd1563306
Mike Jones <mike@mjones.io>
2025-03-19 14:42:33 +0000

    gnu: python-sphinxcontrib-applehelp: Add python-setuptools.
    Change-Id: Id1293351e19b6cfa8b18ed5f62359f503669bed3

--- gnu/packages/sphinx.scm
+++ gnu/packages/sphinx.scm
@@ -308,6 +308,7 @@ sources.")
     (build-system python-build-system)
     (arguments
      `(#:tests? #f))                    ;XXX: circular dependency on Sphinx
+    (native-inputs (list python-setuptools))
     (home-page "https://github.com/sphinx-doc/sphinxcontrib-applehelp")
     (synopsis "Sphinx extension for creating Apple help books")
     (description
