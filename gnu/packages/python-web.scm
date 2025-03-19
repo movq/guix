4d1c69c35d1f31ad76c008583df88d0414c2fda2
Mike Jones <mike@mjones.io>
2025-03-19 18:28:23 +0000

    gnu: python-httptools: Add python-setuptools.
    Change-Id: Ie7d9e23bf8e276ca9618849359939d5c7de18244

--- gnu/packages/python-web.scm
+++ gnu/packages/python-web.scm
@@ -7404,7 +7404,7 @@ and fairly speedy.")
              (substitute* "setup.py" (("Cython==") "Cython>="))
              #t)))))
     (native-inputs
-     (list python-cython python-pytest))
+     (list python-cython python-pytest python-setuptools))
     (inputs
      (list http-parser))
     (home-page "https://github.com/MagicStack/httptools")
