7ea672828f493c01cf43879ecfebc1b113138585
Mike Jones <mike@mjones.io>
2025-03-21 14:33:09 +0000

    gnu: python-factory-boy: Add python-setuptools.
    Change-Id: Ia3af484b86340467f2dd7f5dbce96de15ebdcb4b

--- gnu/packages/python-xyz.scm
+++ gnu/packages/python-xyz.scm
@@ -26873,6 +26873,7 @@ parts of the lines that were modified.")
      ;; Tests are not included in the tarball.
      `(#:tests? #f))
     (propagated-inputs (list python-faker))
+    (native-inputs (list python-setuptools))
     (home-page "https://github.com/benhoyt/scandir")
     (synopsis "Versatile test fixtures replacement")
     (description "Factory_boy is a fixtures replacement based on thoughtbot’s
