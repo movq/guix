83c53666c96707f7f358dc7accf98e2b1bdc5eb4
Mike Jones <mike@mjones.io>
2025-03-27 17:28:01 +0000

    gnu: python-diff-match-patch: Add python-setuptools.
    Change-Id: I77dcbe39f01eea4da12713efd5a651df16d1c52a

--- gnu/packages/python-xyz.scm
+++ gnu/packages/python-xyz.scm
@@ -26371,6 +26371,7 @@ code reviews.")
                       (when tests?
                         (invoke "python" "-m" "unittest"
                                 "diff_match_patch.tests")))))))
+    (native-inputs (list python-setuptools))
     (home-page "https://github.com/diff-match-patch-python/diff-match-patch")
     (synopsis "Synchronize plain text")
     (description "Diff Match and Patch libraries offer robust algorithms to
