00555842d297640a5653755b60892b8ee6418020
Mike Jones <mike@mjones.io>
2025-03-27 16:00:53 +0000

    gnu: python-cycler: Add python-setuptools.
    Change-Id: If70e964adb4d0545101f7cc6cdfa2522f125350e

--- gnu/packages/python-xyz.scm
+++ gnu/packages/python-xyz.scm
@@ -10275,6 +10275,8 @@ transcendental functions).")
      '(#:tests? #f))
     (propagated-inputs
      (list python-six))
+    (native-inputs
+     (list python-setuptools))
     (home-page "https://matplotlib.org/cycler/")
     (synopsis "Composable keyword argument iterator")
     (description
