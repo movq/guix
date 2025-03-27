8f043066ce8591644a0d80410cd43821caef93a9
Mike Jones <mike@mjones.io>
2025-03-27 16:04:54 +0000

    gnu: python-soupsieve: Add python-setuptools.
    Change-Id: Ibbd600690dec18ef89a997dd06d51fe40308ac4e

--- gnu/packages/python-xyz.scm
+++ gnu/packages/python-xyz.scm
@@ -15012,6 +15012,7 @@ converts incoming documents to Unicode and outgoing documents to UTF-8.")
             #~(modify-phases %standard-phases
                 ;; Circular dependency with python-beautifulsoup4.
                 (delete 'sanity-check))))
+    (native-inputs (list python-setuptools))
     (home-page "https://github.com/facelessuser/soupsieve")
     (synopsis "CSS selector library")
     (description
