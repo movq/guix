fc7c9912693cf5fd67c3457719e20640c3302c5b
Mike Jones <mike@mjones.io>
2025-03-28 16:54:38 +0000

    gnu: Add rust-numpy-0.23.
    Change-Id: I94e6988158664796820427d1fb4dd35a062ab7b2

--- gnu/packages/crates-io.scm
+++ gnu/packages/crates-io.scm
@@ -51379,6 +51379,36 @@ giga, kibi.")
 giga, kibi.")
     (license license:expat)))
 
+(define-public rust-numpy-0.23
+  (package
+    (name "rust-numpy")
+    (version "0.23.0")
+    (source
+     (origin
+       (method url-fetch)
+       (uri (crate-uri "numpy" version))
+       (file-name (string-append name "-" version ".tar.gz"))
+       (sha256
+        (base32 "0y1yw681b9vhaihmq15qa5h5af0yi6iyc1mg6dys167r0plalk5r"))))
+    (build-system cargo-build-system)
+    (arguments
+     `(#:cargo-inputs (("rust-half" ,rust-half-2)
+                       ("rust-libc" ,rust-libc-0.2)
+                       ("rust-nalgebra" ,rust-nalgebra-0.32)
+                       ("rust-ndarray" ,rust-ndarray-0.16)
+                       ("rust-num-complex" ,rust-num-complex-0.4)
+                       ("rust-num-integer" ,rust-num-integer-0.1)
+                       ("rust-num-traits" ,rust-num-traits-0.2)
+                       ("rust-pyo3" ,rust-pyo3-0.23)
+                       ("rust-rustc-hash" ,rust-rustc-hash-2))
+       #:cargo-development-inputs (("rust-nalgebra" ,rust-nalgebra-0.32)
+                                   ("rust-pyo3" ,rust-pyo3-0.23))))
+    (home-page "https://github.com/PyO3/rust-numpy")
+    (synopsis "PyO3-based Rust bindings of the NumPy C-API")
+    (description
+     "This package provides @code{PyO3-based} Rust bindings of the @code{NumPy} C-API.")
+    (license license:bsd-2)))
+
 (define-public rust-numpy-0.22
   (package
     (name "rust-numpy")
