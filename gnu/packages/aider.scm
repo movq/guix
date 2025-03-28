999e0ee656c102218a6e48d312b20c817d59ff8a
Mike Jones <mike@mjones.io>
2025-03-28 17:12:23 +0000

    !wip aider
    Change-Id: Ice13d674bf52c969f185dc0ee0e93f063ca8d64b

--- gnu/packages/aider.scm
+++ gnu/packages/aider.scm
@@ -19,10 +19,15 @@
 (define-module (gnu packages aider)
   #:use-module ((guix licenses) #:prefix license:)
   #:use-module (gnu packages)
+  #:use-module (gnu packages check)
   #:use-module (gnu packages crates-io)
+  #:use-module (gnu packages databases)
+  #:use-module (gnu packages digest)
   #:use-module (gnu packages tree-sitter)
   #:use-module (gnu packages python)
   #:use-module (gnu packages python-build)
+  #:use-module (gnu packages python-crypto)
+  #:use-module (gnu packages python-science)
   #:use-module (gnu packages python-web)
   #:use-module (gnu packages python-xyz)
   #:use-module (gnu packages rust)
@@ -88,7 +93,6 @@
       #:cargo-inputs `(("rust-crossbeam-channel" ,rust-crossbeam-channel-0.5)
                        ("rust-notify" ,rust-notify-7)
                        ("rust-pyo3" ,rust-pyo3-0.23))))
-
     (propagated-inputs (list python-anyio))
     (native-inputs (list maturin
                          python-wrapper))
@@ -144,10 +148,313 @@
                              python-pyperclip
                              python-pyyaml
                              python-tqdm
-                             python-watchfiles))
+                             python-watchfiles
+                             python-wcwidth
+                             python-yarl))
     (native-inputs (list python-setuptools))
     (home-page "https://github.com/paul-gauthier/aider")
     (synopsis "AI programming assistant")
     (description
      "This package provides a terminal-based AI programming assistant.")
     (license license:asl2.0)))
+
+(define-public python-tiktoken
+  (package
+    (name "python-tiktoken")
+    (version "0.9.0")
+    (source
+     (origin
+       (method url-fetch)
+       (uri (pypi-uri "tiktoken" version))
+       (sha256
+        (base32 "0p9cg6n8mzdi4lbbwxrrp26chy5hr14bqmzr3w74kq1qm6k5qanh"))))
+    (build-system cargo-build-system)
+    (arguments
+     (list
+      #:imported-modules `(,@%cargo-build-system-modules
+                           ,@%pyproject-build-system-modules)
+      #:modules '((guix build cargo-build-system)
+                  ((guix build pyproject-build-system)
+                   #:prefix py:)
+                  (guix build utils))
+      #:phases #~(modify-phases %standard-phases
+                   (replace 'build
+                     (assoc-ref py:%standard-phases
+                                'build))
+                   (delete 'package)
+                   (replace 'install
+                     (assoc-ref py:%standard-phases
+                                'install)))
+      #:cargo-inputs
+      `(("rust-bstr" ,rust-bstr-1)
+        ("rust-fancy-regex" ,rust-fancy-regex-0.13)
+        ("rust-pyo3" ,rust-pyo3-0.22))))
+    (propagated-inputs (list python-regex python-requests))
+    (native-inputs
+     (list python-setuptools
+           python-setuptools-rust
+           python-wheel
+           python-wrapper))
+    (home-page #f)
+    (synopsis "tiktoken is a fast BPE tokeniser for use with OpenAI's models")
+    (description
+     "tiktoken is a fast BPE tokeniser for use with @code{OpenAI's} models.")
+    (license #f)))
+
+(define-public python-sse-starlette
+  (package
+    (name "python-sse-starlette")
+    (version "2.2.1")
+    (source
+     (origin
+       (method url-fetch)
+       (uri (pypi-uri "sse_starlette" version))
+       (sha256
+        (base32 "06dl330s5i7isw8kp58xhpm7kcxl105l6wylnbbfwji735ghsisl"))))
+    (build-system pyproject-build-system)
+    (propagated-inputs (list python-anyio python-starlette))
+    (native-inputs (list python-setuptools python-wheel))
+    (home-page #f)
+    (synopsis "SSE plugin for Starlette")
+    (description "SSE plugin for Starlette.")
+    (license #f)))
+
+(define-public python-litellm
+  (package
+    (name "python-litellm")
+    (version "1.64.1")
+    (source
+     (origin
+       (method url-fetch)
+       (uri (pypi-uri "litellm" version))
+       (sha256
+        (base32 "12cgxkky7zyh990vmhyr0z14hgy5axv9c7k99pd7mmzvn68wifkk"))))
+    (build-system pyproject-build-system)
+    (propagated-inputs (list python-aiohttp
+                             python-click
+                             python-httpx
+                             python-importlib-metadata
+                             python-jinja2
+                             python-jsonschema
+                             python-openai
+                             python-pydantic
+                             python-dotenv
+                             python-tiktoken
+                             python-tokenizers))
+    (native-inputs (list python-poetry-core python-wheel))
+    (home-page #f)
+    (synopsis "Library to easily interface with LLM API providers")
+    (description "Library to easily interface with LLM API providers.")
+    (license license:expat)))
+
+(define-public python-fastavro
+  (package
+    (name "python-fastavro")
+    (version "1.10.0")
+    (source
+     (origin
+       (method url-fetch)
+       (uri (pypi-uri "fastavro" version))
+       (sha256
+        (base32 "0vw8ssjh2lbn286r0fk6gfrj24rj0al7b3587m5gxkajdnn43gs7"))))
+    (build-system pyproject-build-system)
+    (native-inputs (list python-cython python-setuptools python-wheel))
+    (home-page "https://github.com/fastavro/fastavro")
+    (synopsis "Fast read/write of AVRO files")
+    (description "Fast read/write of AVRO files.")
+    (license license:expat)))
+
+(define-public python-httpx-sse
+  (package
+    (name "python-httpx-sse")
+    (version "0.4.0")
+    (source
+     (origin
+       (method url-fetch)
+       (uri (pypi-uri "httpx-sse" version))
+       (sha256
+        (base32 "08cp2dhzjlxpba8yrmjyyhbhixxm5va9wlnks6nj5qqc0yis708y"))))
+    (build-system pyproject-build-system)
+    (native-inputs (list python-setuptools python-setuptools-scm python-wheel))
+    (propagated-inputs (list python-httpx))
+    (home-page "")
+    (synopsis "Consume Server-Sent Event (SSE) messages with HTTPX.")
+    (description "Consume Server-Sent Event (SSE) messages with HTTPX.")
+    (license license:expat)))
+
+(define-public python-faiss-cpu
+  (package
+    (name "python-faiss-cpu")
+    (version "1.10.0")
+    (source
+     (origin
+       (method url-fetch)
+       (uri (pypi-uri "faiss_cpu" version))
+       (sha256
+        (base32 "17a7dvacq8yajvj8vryl4qhqs6y9di6mm2kzsvs3dh2by9asbp2v"))))
+    (build-system pyproject-build-system)
+    (propagated-inputs (list python-numpy python-packaging))
+    (native-inputs (list python-numpy python-setuptools python-wheel))
+    (home-page #f)
+    (synopsis
+     "A library for efficient similarity search and clustering of dense vectors.")
+    (description
+     "This package provides a library for efficient similarity search and clustering
+of dense vectors.")
+    (license license:expat)))
+
+(define-public python-tokenizers
+  (package
+    (name "python-tokenizers")
+    (version "0.21.1")
+    (source
+     (origin
+       (method git-fetch)
+       (uri (git-reference
+             (url "https://github.com/huggingface/tokenizers.git")
+             (commit (string-append "v" version))))
+       (sha256
+        (base32 "0q0v2p9r201n1a12rni6qbc0814ynzx66y46is6hr7v7lq4xjbnx"))))
+    (build-system cargo-build-system)
+    (arguments
+     (list
+      #:imported-modules `(,@%cargo-build-system-modules
+                           ,@%pyproject-build-system-modules)
+      #:modules '((guix build cargo-build-system)
+                  ((guix build pyproject-build-system)
+                   #:prefix py:)
+                  (guix build utils))
+      #:phases #~(modify-phases %standard-phases
+                   (add-after 'unpack 'cd-to-bindings-dir
+                     (lambda _
+                       (chdir "bindings/python")))
+                   (replace 'build
+                     (assoc-ref py:%standard-phases
+                                'build))
+                   (delete 'package)
+                   (replace 'install
+                     (assoc-ref py:%standard-phases
+                                'install)))
+      #:cargo-development-inputs
+      `(("rust-serde" ,rust-serde-1)
+        ("rust-serde-derive" ,rust-serde-derive-1))
+      #:cargo-inputs `(
+                       ("rust-derive-builder" ,rust-derive-builder-0.20)
+                       ("rust-env-logger" ,rust-env-logger-0.11)
+                       ("rust-esaxx-rs" ,rust-esaxx-rs-0.1)
+                       ("rust-indicatif" ,rust-indicatif-0.17)
+                       ("rust-itertools" ,rust-itertools-0.12)
+                       ("rust-macro-rules-attribute" ,rust-macro-rules-attribute-0.2)
+                       ("rust-monostate" ,rust-monostate-0.1)
+                       ("rust-ndarray" ,rust-ndarray-0.16)
+                       ("rust-numpy" ,rust-numpy-0.23)
+                       ("rust-pyo3" ,rust-pyo3-0.23)
+                       ("rust-rand" ,rust-rand-0.8)
+                       ("rust-rayon" ,rust-rayon-1)
+                       ("rust-rayon-cond" ,rust-rayon-cond-0.3)
+                       ("rust-regex" ,rust-regex-1)
+                       ("rust-regex-syntax" ,rust-regex-syntax-0.8)
+                       ("rust-spm-precompiled" ,rust-spm-precompiled-0.1)
+                       ("rust-unicode-normalization-alignments"
+                        ,rust-unicode-normalization-alignments-0.1))))
+    (propagated-inputs (list python-huggingface-hub python-huggingface-hub))
+    (native-inputs (list python-black
+                         maturin
+                         python-numpy
+                         python-pytest
+                         python-requests
+                         python-setuptools
+                         python-wrapper))
+    (home-page #f)
+    (synopsis #f)
+    (description #f)
+    (license #f)))
+
+(define-public python-cohere
+  (package
+    (name "python-cohere")
+    (version "5.14.0")
+    (source
+     (origin
+       (method url-fetch)
+       (uri (pypi-uri "cohere" version))
+       (sha256
+        (base32 "1zbqbkk1c1y7p5bswpivbl2w5i29s5pyvy4zyz4796q4ai62vgzx"))))
+    (build-system pyproject-build-system)
+    (propagated-inputs (list python-fastavro
+                             python-httpx
+                             python-httpx-sse
+                             python-pydantic
+                             python-pydantic-core
+                             python-requests
+                             python-tokenizers
+                             python-types-requests
+                             python-typing-extensions))
+    (native-inputs (list python-poetry-core))
+    (home-page #f)
+    (synopsis #f)
+    (description #f)
+    (license license:expat)))
+
+(define-public python-ml-dtypes
+  (package
+    (name "python-ml-dtypes")
+    (version "0.5.1")
+    (source
+     (origin
+       (method url-fetch)
+       (uri (pypi-uri "ml_dtypes" version))
+       (sha256
+        (base32 "1j82c0jnx1xdrddalqmbdc5zjj9j06w4x66nis29ajmqkdamhnxc"))))
+    (build-system pyproject-build-system)
+    (propagated-inputs (list python-numpy))
+    (native-inputs (list python-absl-py
+                         python-numpy
+                         python-pyink
+                         python-pylint
+                         python-pytest
+                         python-pytest-xdist
+                         python-setuptools
+                         python-wheel))
+    (home-page #f)
+    (synopsis #f)
+    (description #f)
+    (license #f)))
+
+(define-public python-redisvl
+  (package
+    (name "python-redisvl")
+    (version "0.4.1")
+    (source
+     (origin
+       (method url-fetch)
+       (uri (pypi-uri "redisvl" version))
+       (sha256
+        (base32 "1rh1cill5hb299wj7ssqrkilwb9365f9286axz094ix9dd13cspx"))))
+    (build-system pyproject-build-system)
+    (propagated-inputs (list python-boto3
+                             python-cohere
+                             python-coloredlogs
+                             ;python-google-cloud-aiplatform
+                             ;python-mistralai
+                             python-ml-dtypes
+                             python-numpy
+                             python-openai
+                             python-protobuf
+                             python-pydantic
+                             python-ulid
+                             python-pyyaml
+                             python-redis
+                             python-scipy
+                             python-sentence-transformers
+                             python-tabulate
+                             python-tenacity
+                             python-voyageai))
+    (native-inputs (list python-poetry-core))
+    (home-page "https://github.com/redis/redis-vl-python")
+    (synopsis
+     "Python client library and CLI for using Redis as a vector database")
+    (description
+     "Python client library and CLI for using Redis as a vector database.")
+    (license license:expat)))
+
