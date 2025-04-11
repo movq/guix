4a78ffcdaf92f916f5af5db05ec1524eccf96bda
Mike Jones <mike@mjones.io>
2025-04-11 12:26:26 +0100

    gnu: qtlocation-5: Fix build with gcc-14.
    Change-Id: Ib7366d5d5d962e94d4a4ea3e4307861ad0a79743

--- gnu/local.mk
+++ gnu/local.mk
@@ -2040,6 +2040,7 @@ dist_patch_DATA =						\
   %D%/packages/patches/qcodeeditor-qt6.patch			\
   %D%/packages/patches/qtdeclarative-5-disable-qmlcache.patch	\
   %D%/packages/patches/qtdeclarative-disable-qmlcache.patch	\
+  %D%/packages/patches/qtlocation-5-fix-build-with-gcc-14.patch \
   %D%/packages/patches/quodlibet-fix-invalid-glob.patch	\
   %D%/packages/patches/quodlibet-fix-mtime-tests.patch		\
   %D%/packages/patches/qucs-s-qucsator-rf-search.patch		\
