519e26fabab1535197b2fdb8f5fc2b153515b220
Mike Jones <mike@mjones.io>
2025-04-15 18:51:18 +0100

    gnu: python-subunit-bootstrap: Add python-setuptools.
    Change-Id: I8b16aea413a0280d2dad1421d074ef35422aa61d

--- gnu/packages/check.scm
+++ gnu/packages/check.scm
@@ -2327,6 +2327,7 @@ use of resources by test cases.")))
      (list python-extras python-testtools-bootstrap))
     (native-inputs
      (list python-fixtures-bootstrap python-hypothesis
+           python-setuptools
            python-testscenarios-bootstrap))
     (home-page "https://launchpad.net/subunit")
     (synopsis "Python implementation of the subunit protocol")
